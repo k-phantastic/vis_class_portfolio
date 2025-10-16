f726fc90b2cb4c6743aae1b526882853027d513b
Khanh Phan <117617878+khp023@users.noreply.github.com>
2025-10-16 13:35:32 -0700

    JS

---
+++ global.js
@@ -0,0 +1,100 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname
+// );
+
+// if (currentLink) {
+//   // or if (currentLink !== undefined)
+//   currentLink?.classList.add('current');
+// }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'https://github.com/k-phantastic', title: 'GitHub' },
+    // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/website/";         // GitHub Pages repo name
+
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // if (!ARE_WE_HOME && !url.startsWith('http')) {
+    //     url = '../' + url;
+    // }
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    // TODO create link and add it to nav
+    // Create link and add it to nav
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+            <option value="light dark">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+		</select>
+	</label>`
+);
+
+let select = document.querySelector("select");
+
+select.addEventListener('input', function (event) {
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
+    localStorage.colorScheme = event.target.value
+});
+
+if ("colorScheme" in localStorage) {
+    let saved = localStorage.colorScheme;
+    document.documentElement.style.setProperty('color-scheme', saved);
+    select.value = saved;
+}
+
+let form = document.querySelector('form');
+form?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const data = new FormData(form);
+    let form_url = form.action + "?";
+    let first = true;
+    for (let [name, value] of data) {
+        // TODO build URL parameters here
+        //  mailto:leaverou@mit.edu?subject=Hello&body=Sup?
+        if (!first) form_url += "&";
+        form_url += name + "=" + encodeURIComponent(value);
+        first = false;
+    }
+    console.log(form_url);
+    location.href = form_url;
+});
