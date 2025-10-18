d51c6ca00d40b7417e4b7498bf08589921247a4c
Khanh Phan <117617878+khp023@users.noreply.github.com>
2025-10-17 20:00:59 -0700

    Edits to javascript

--- global.js
+++ global.js
@@ -51,12 +51,15 @@ for (let p of pages) {
     //     a.classList.add('current');
     // }
     if (a.host === location.host) {
-        const currentPath = location.pathname.endsWith('/') ? location.pathname + 'index.html' : location.pathname;
-        const linkPath = a.pathname.endsWith('/') ? a.pathname + 'index.html' : a.pathname;
-        if (linkPath === currentPath) {
-            a.classList.add('current');
-        }
+        a.classList.add('current');
     }
+    // if (a.host === location.host) {
+    //     const currentPath = location.pathname.endsWith('/') ? location.pathname + 'index.html' : location.pathname;
+    //     const linkPath = a.pathname.endsWith('/') ? a.pathname + 'index.html' : a.pathname;
+    //     if (linkPath === currentPath) {
+    //         a.classList.add('current');
+    //     }
+    // }
     if (a.host !== location.host) {
         a.target = '_blank';
     }
